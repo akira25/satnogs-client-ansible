bf1172ca0116b83969aea9d41ea7938c25da8726
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2017-09-27 01:44:42 +0300

    Allow overriding of 'satnogs-client' version and Git URL
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- roles/satnogs-setup/files/usr/local/share/satnogs-setup/config.sh
+++ roles/satnogs-setup/files/usr/local/share/satnogs-setup/config.sh
@@ -65,6 +65,8 @@ ENABLE_DECODED_DATA:Enable decoded data:yesno
 SATNOGS_RADIO_GR_SATNOGS_PACKAGE:Define gr-satnogs package:input
 HAMLIB_UTILS_RIG_ENABLED:Enable Hamlib rigctld:yesno
 HAMLIB_UTILS_RIG_OPTS:Define Hamlib rigctld options:input
+SATNOGS_CLIENT_VERSION:Define SatNOGS client version:input
+SATNOGS_CLIENT_URL:Define SatNOGS client Git URL:input
 SATNOGS_SETUP_ANSIBLE_URL:Define Ansible Git URL:input"
 
 to_lower() {
